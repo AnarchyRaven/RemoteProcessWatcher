5cccce400a3ce203d0d025fe4fa26188dc45b804
Emre Rauhofer <47245944+AnarchyRaven@users.noreply.github.com>
2019-06-14 11:01:39 +0200

    The NetworkLibrary

---
+++ NetworkLibrary/ProcessListContainer.cs
@@ -0,0 +1,50 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ProcessListContainer.cs" company="FH Wiener Neustadt">
+//     Copyright (c) Emre Rauhofer. All rights reserved.
+// </copyright>
+// <author>Emre Rauhofer</author>
+// <summary>
+// This is a network library.
+// </summary>
+//-----------------------------------------------------------------------
+namespace NetworkLibrary
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The <see cref="ProcessListContainer"/> class.
+    /// </summary>
+    [Serializable]
+    public class ProcessListContainer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProcessListContainer"/> class.
+        /// </summary>
+        public ProcessListContainer()
+        {
+            this.OldProcesses = new List<ProcessContainer>();
+            this.NewProcesses = new List<ProcessContainer>();
+        }
+
+        /// <summary>
+        /// Gets or sets a list of the old processes.
+        /// </summary>
+        /// <value> A list of processes. </value>
+        public List<ProcessContainer> OldProcesses
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a list of the new processes.
+        /// </summary>
+        /// <value> A list of processes. </value>
+        public List<ProcessContainer> NewProcesses
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ NetworkLibrary/ProcessListEventArgs.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ProcessListEventArgs.cs" company="FH Wiener Neustadt">
+//     Copyright (c) Emre Rauhofer. All rights reserved.
+// </copyright>
+// <author>Emre Rauhofer</author>
+// <summary>
+// This is a network library.
+// </summary>
+//-----------------------------------------------------------------------
+namespace NetworkLibrary
+{
+    using System;
+
+    /// <summary>
+    /// The <see cref="ProcessListEventArgs"/> class.
+    /// </summary>
+    public class ProcessListEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProcessListEventArgs"/> class.
+        /// </summary>
+        /// <param name="container"> The list of the new and old processes. </param>
+        public ProcessListEventArgs(ProcessListContainer container)
+        {
+            this.List = container;
+        }
+
+        /// <summary>
+        /// Gets the list of new and old processes.
+        /// </summary>
+        /// <value> A normal <see cref="ProcessListContainer"/> class. </value>
+        public ProcessListContainer List
+        {
+            get;
+            private set;
+        }
+    }
+}
